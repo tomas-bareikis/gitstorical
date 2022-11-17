202cd1789f0fea914f8c18052408baaa4f5f066b
Tomas Bareikis <tomas.bareikis.dev@gmail.com>
2022-11-17 19:38:58 +0200

    Clone to temp dir

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/go-git/go-git/v5"
 )
 
 func main() {
-	_, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+	tempPath, err := os.MkdirTemp("", "gitstorical")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("created dir", tempPath)
+
+	defer os.RemoveAll(tempPath)
+
+	_, err = git.PlainClone(tempPath, false, &git.CloneOptions{
 		URL:      "https://github.com/go-git/go-git",
 		Progress: os.Stdout,
 	})
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
