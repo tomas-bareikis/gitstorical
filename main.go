d5435b7a2c22209f7aab2857f9dc2b1533c705cb
Tomas Bareikis <tomas.bareikis.dev@gmail.com>
2022-11-17 19:31:19 +0200

    Example git clone

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
 
 func main() {
-	fmt.Println("hello world")
+	_, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+		URL:      "https://github.com/go-git/go-git",
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		panic(err)
+	}
 }
